365a83d5033951b8b4bce70bbb6fa879af9186f8
Rayooo <chenxiaolei.ray@gmail.com>
2016-05-24 22:06:13 +0800

    优化图片上传

---
+++ src/servlet/UploadFile.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Ray on 16/5/24.
+ */
+@WebServlet(name = "UploadFile")
+public class UploadFile extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}

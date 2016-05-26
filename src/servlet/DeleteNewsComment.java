79878f653e5296e916892f39d4f0813ef1c92eea
Rayooo <chenxiaolei.ray@gmail.com>
2016-05-26 19:19:11 +0800

    完成删除新闻评论模块

---
+++ src/servlet/DeleteNewsComment.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Ray on 16/5/26.
+ */
+@WebServlet(name = "DeleteNewsComment")
+public class DeleteNewsComment extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}

b56dd6b78d91fdbed247501ad2c7b813dcc9718b
Rayooo <chenxiaolei.ray@gmail.com>
2016-06-02 21:34:07 +0800

    删除用户时将他所有成果都删除

--- src/servlet/DeleteUser.java
+++ src/servlet/DeleteUser.java
@@ -24,29 +24,46 @@ public class DeleteUser extends HttpServlet {
             return;
         String userId = request.getParameter("userId");
         HttpSession session = request.getSession();
+        PrintWriter writer = response.getWriter();
+        boolean isError = true;
         if(Integer.parseInt(userId) == (Integer)session.getAttribute("userId")){
             return;
         }
+        DbConnection dbConnection = new DbConnection();
         try {
-            DbConnection dbConnection = new DbConnection();
+            //开始一个事务
+            dbConnection.connection.setAutoCommit(false);
+
             Statement statement = dbConnection.connection.createStatement();
             String sql = "UPDATE user SET isDeleted=1 WHERE id="+ userId;
             int rs = statement.executeUpdate(sql);
-            PrintWriter writer = response.getWriter();
+
             if(rs > 0){
-                writer.print("success");
+                //将其所有的成果都删除
+                sql = "UPDATE achievement SET isDeleted=1 WHERE userId="+userId;
+                int rs2 = statement.executeUpdate(sql);
+                if(rs2 > 0){
+                    writer.print("success");
+                    dbConnection.connection.commit();
+                    isError = false;
+                }
             }
-            else{
+            if(isError){
                 writer.print("error");
+                dbConnection.connection.rollback();
             }
             writer.flush();
             dbConnection.closeConnection();
         }
         catch (SQLException e) {
 //        e.printStackTrace();
-            PrintWriter writer = response.getWriter();
             writer.print("error");
             writer.flush();
+            try {
+                dbConnection.connection.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
         }
     }
 
